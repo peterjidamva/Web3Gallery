55680a6f504d0d42806f868e713b110812982850
peterjidamva <jidamvap@gmail.com>
2022-03-23 18:14:17 +0300

    valueess

---
+++ lib/pairs/view_pairs.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ViewAvailableCryptoPairs extends StatefulWidget {
+  const ViewAvailableCryptoPairs({Key? key}) : super(key: key);
+
+  @override
+  State<ViewAvailableCryptoPairs> createState() => _ViewAvailableCryptoPairsState();
+}
+
+class _ViewAvailableCryptoPairsState extends State<ViewAvailableCryptoPairs> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
